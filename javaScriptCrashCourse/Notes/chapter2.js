959dfc38652afa2caa331d76bebd722e2bef2850
Cperez5401 <cperez5401@gmail.com>
2024-02-17 00:46:11 -0800

    added to java notes

---
+++ javaScriptCrashCourse/Notes/chapter2.js
@@ -0,0 +1,37 @@
+23424234 // this is a number
+
+'hello world' //this is a string
+
+null
+
+undefined
+
+true&&false //these are booleans
+
+let friends = {
+  chris:'is so cool',
+  lucy: 'not so cool',
+  numberOfEggs:3
+}
+
+//this is an object dictionary^^
+
+
+let exampleArray = [1,2,3,4,5,6,7,8,9]
+
+// this is an array^^
+
+
+function multiplyNumbers(value, secondValue) {
+  //multiplies 2 numbers
+  console.log(value * secondValue)
+}
+
+let condition1 = true
+let condition2 = false
+
+if (condition1) {
+  console.log('hi world')
+}
+
+myNewFunction(2,4)
