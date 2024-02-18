def1a8685ef5547e7257e67abb7142cfa180eb74
Cperez5401 <cperez5401@gmail.com>
2024-02-18 07:58:35 -0800

    updated js files

--- javaScriptCrashCourse/Notes/chapter3.js
+++ javaScriptCrashCourse/Notes/chapter3.js
@@ -1,3 +1,30 @@
 // data manipulation
 
-let exampleSentence = "this is a string";
+let exampleSentence = "this is a string"
+
+let stringLength = exampleSentence.length
+
+// console.log('value at the end of the string = ', exampleSentence[stringLength - 1])
+
+// let combinedString =
+
+// console.log(combinedString)
+
+const splitSentence = exampleSentence.length
+
+//regex
+
+// console.log(splitSentence)
+//Arrays and lists
+let simpleArray = ["chris", "is", "cool"]
+
+//crud - create read update delete
+
+console.log(simpleArray)
+
+// pop push shift unshift
+
+simpleArray.push('new word')
+
+
+console.log(simpleArray)
